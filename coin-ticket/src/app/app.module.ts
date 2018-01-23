427923336428d02cd6b118f8a1d5c5af22a5404a
gerardcebriaVN <gcebriai@viewnext.com>
2018-01-23 23:56:34 +0100

    recoge datos de coinmarketcap

--- coin-ticket/src/app/app.module.ts
+++ coin-ticket/src/app/app.module.ts
@@ -1,19 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { CoinmarketcapService } from './coinmarketcap.service';
+import { ChartsModule } from 'ng2-charts';
+import { ChartsComponent } from './menu/charts/charts/charts.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    ChartsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ChartsModule,
+    HttpModule
   ],
   providers: [CoinmarketcapService],
   bootstrap: [AppComponent]
--- coin-ticket/src/app/coinmarketcap.service.ts
+++ coin-ticket/src/app/coinmarketcap.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Injectable()
 export class CoinmarketcapService {
+  apiPath: string;
+  results;
+  constructor(private http: Http) {
+    this.apiPath = 'https://api.coinmarketcap.com/v1/ticker/';
+  }
 
-  constructor() { }
+  getData(){
+
+  }
+
 
 }
